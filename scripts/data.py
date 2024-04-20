1164db79cc405625cf1d5a281b8a269e6f99cff1
junesirius <wojiaoziyou@gmail.com>
2024-04-20 18:18:49 -0400

    Update StoryText to Step_13

--- scripts/data.py
+++ scripts/data.py
@@ -7,7 +7,7 @@ v_love_ending_5: int = 1  # Ending_5
 v_intelligence_ending_5: int = 5  # Ending_5 (TODO: TBD)
 order_num_low_judge_7: float = 30  # Judge_7 (TODO: TBD)
 order_num_high_judge_7: float = 40  # Judge_7 (TODO: TBD)
-o_intelligence_judge_8_2: int = 5  # Judge_8_2 (TODO: TBD)
+o_intelligence_judge_8_1: int = 5  # Judge_8_1 (TODO: TBD)
 o_love_judge_9: int = 4  # Judge_9 (TODO: TBD)
 
 # Variables initialized at beginning
@@ -31,8 +31,8 @@ s_strength: int = 10
 s_emotion: int = 0
 
 # Flag values
-tip_flag: bool = False  # Judge_1_1
 activity_cnt: int = 0  # Choice_1
+tip_flag: bool = False  # Judge_1_1
 music_flag: bool = False  # Choice_1_2
 painting_flag: bool = False  # Choice_1_3
 poetry_flag: bool = False  # Choice_1_4
@@ -42,7 +42,9 @@ punish_cnt: int = 0  # Choice_2_2, Judge_2_2
 attention_flag: bool = False  # Choice_2_2, Judge_2_2
 photo_flag: bool = False  # Step 4
 camera_flag: bool = False  # Step_5_1
-monthly_ranking_flag: bool = False  # Step_13_2
+
+v_name_flag: bool = False  # Step 12
+v_love_feeling_flag: bool = False  # Step12-1
 
 # Stage 6-7 loops
 order_num: int = 10  # Choice_6
